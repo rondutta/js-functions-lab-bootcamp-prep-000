b39a32c8c871a0cbb3207a8aed77f37b3328f750
Rayan Dutta <rondutta23@gmail.com>
2018-05-15 03:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`;
+function happyHolidayTo(name,holiday){
+  return `Happy ${holiday}, ${name}!`;
 }
